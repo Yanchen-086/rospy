36be6d231be69d37da738c693c5953f1077febec
Yanchen-086 <81214476+Yanchen-086@users.noreply.github.com>
2022-03-20 16:42:46 +0800

    Add files via upload

--- aa.py
+++ aa.py
@@ -1,8 +1,67 @@
 
 
-from rospy import libhelloworldpy
 
-print(libhelloworldpy.add_1(1,2))
-print(libhelloworldpy.add(1,2))
-libhelloworldpy.sayhello()
-print(libhelloworldpy.sayhello())
+
+
+from myrospy import rospy
+import logging
+import logging.config
+
+#string="chatter"
+
+#print(rospy.init_node("talker", True))
+#print(rospy.Publisher("chatter",string))
+#print(rospy.get_time())
+#print(rospy.is_shutdown())
+
+#str="helloworld"
+#print(rospy.loginfo(str))
+#print(rospy.publish(str))
+#print(rospy.sleep())rospy.sleep())
+#print(rospy.sleep()rospy.sleet())
+
+
+
+#data="aa"
+#rospy.loginfo('I heard')
+#print(rospy.init_node("chatter"))
+
+#logging.config.fileConfig('python_logging.config')
+#logger=logging.getLogger(name='log')
+rospy.init_node('log',anonymous=False)
+ logging.debug('this is debug message')  
+ logging.info('this is info message')  
+ logging.warning('this is warning message') 
+
+
+rospy.loginfo('I')
+
+#logger.debug('msg')
+#logger.info('msg')
+
+
+
+
+
+##print(rospy.Subscriber("chatter", str))
+##print(rospy.spin())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
